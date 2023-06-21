85926a5c86e313e867037f4083f278e42f4c1ea8
vishal <vishrrajput02@gmail.com>
2023-06-21 22:54:18 +0530

    any base subtraction problem

--- src/anyBaseSubtraction.java
+++ src/anyBaseSubtraction.java
@@ -1,42 +1,45 @@
 import java.util.Scanner;
 
-public class anyBaseAddition {
+public class anyBaseSubtraction {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int b = scn.nextInt();
 		int n1 =scn.nextInt();
 		int n2 = scn.nextInt();
-		int d =valueOfAddition(b,n1,n2);
+		int d =valueOfSubtraction(b,n1,n2);
 		System.out.println(d);
 	}
 	
 	
-	public static int valueOfAddition(int b, int n1 ,int n2) {
+	public static int valueOfSubtraction(int b ,int n1,int n2) {
 		int rv=0;
 		int c=0;
 		int p=1;
-		while(n1>0 || n2>0 || c>0) {
-			
-			
+		while(n2>0) {
 			int d1=n1%10;
 			int d2=n2%10;
 			n1=n1/10;
 			n2=n2/10;
 			
-			
-			int ds=d1+d2+c;
-			c=ds/b;
-			ds=ds%b;
-			
-			
-			rv=rv+ds*p;
+			int d=0;
+			d2=d2+c;
+			if(d2>=d1) {
+				c=0;
+				d=d2-d1;
+			}else {
+				c=-1;
+				d=d2+b-d1;
+			}
+					
+			rv=rv+d*p;
 			p=p*10;
-			
+					
 		}
 		
 		
 		return rv;
 	}
+	
 
 }
